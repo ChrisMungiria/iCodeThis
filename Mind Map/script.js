f52a00394b89b8220a394148e97aea6323cbc730
Chris Mungiria <christopher.mungiria@strathmore.edu>
2024-03-09 18:12:29 +0300

    Mind map

---
+++ Mind Map/script.js
@@ -0,0 +1,11 @@
+const intuitive = document.getElementById("intuitive");
+const memory = document.getElementById("memory");
+const collaboration = document.getElementById("collaboration");
+const focus = document.getElementById("focus");
+const middle = document.getElementById("middle");
+
+new LeaderLine(intuitive, middle, {
+  startPlugColor: "#1a6be0",
+  endPlugColor: "#1efdaa",
+  gradient: true,
+});
